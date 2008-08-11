de2439f6b1a60635ab760b060274df58429f8287
bisp <bisp@87372704-afca-ba4a-94b7-cf6f7f32583b>
2008-08-11 18:26:55 +0000

    File renamed (my_interrupt.c -> interrupt.c)
    git-svn-id: file:///home/simeon/FH/Diplom/VMC Vertiefung Microcontroller Wahlplflichtfach/Smarties2/software/svn_repository@23 87372704-afca-ba4a-94b7-cf6f7f32583b

---
+++ smarties/trunk/src/interrupt.c
@@ -0,0 +1,32 @@
+#include "smarties2.h"
+#include "avr/interrupt.h"
+
+/**
+ * @file my_interrupt.c 
+ * 
+ * Alle necessary interrupt routines. Please check the source code of
+ * this file for more documentation.
+ * 
+ */
+
+extern smartie_sorter ss;
+
+ISR (TIMER0_OVF_vect)
+// Interrupt routine, executed every millisecond
+{	
+	/* user input (rotary encoder) stuff */
+	rotary_encoder_stuff();
+	
+	/* light barrier stuff */
+	lightbarrier_stuff ();
+	
+	// display stuff
+	// sensor stuff
+	// engine stuff
+	//engine_stuff();
+	
+	// shaker stuff
+	//shaker_stuff();
+	
+	
+}
